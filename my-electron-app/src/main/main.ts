dd686fd6a9a9b34f2c3b8fb3cd5e68fa88d2eac4
pon-nuki <lion360813@yahoo.co.jp>
2025-06-03 13:12:00 +0900

    ウィンドウにアプリアイコンを追加

--- my-electron-app/src/main/main.ts
+++ my-electron-app/src/main/main.ts
@@ -16,6 +16,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, '..', '..', 'icon.ico'),
     show: false,
     webPreferences: {
       preload: preloadPath,
